c79200d3340283c97836e32e63f9c6e2307571f0
nelvinc <nelvinsanchezcemas@gmail.com>
2021-05-11 20:58:51 -0400

    Loading with initializing

--- src/App.js
+++ src/App.js
@@ -7,8 +7,17 @@ function App() {
 
   const { user, initializing } = useAuthState(firebase.auth());
 
+  const renderLoading = () => {
+    if (initializing) {
+      return <div>
+        <h1>Loading ...</h1>
+      </div>
+    }
+  }
+
   return (
     <div>
+      { renderLoading()}
       {
         user ? (
           <>
