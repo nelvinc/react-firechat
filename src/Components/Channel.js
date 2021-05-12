853b4e7389a3b105f5e895594fdee839e44bb5b4
nelvinc <nelvinsanchezcemas@gmail.com>
2021-05-11 22:43:52 -0400

    Add service Messenger

--- src/Components/Channel.js
+++ src/Components/Channel.js
@@ -1,5 +1,6 @@
-import { useEffect, useState } from "react";
-import { db } from "../config/firebase";
+import { useEffect, useState, useRef } from "react";
+import { db, firebase } from "../config/firebase";
+import  Message from "./Message";
 
 const Channel = ({ user = null }) => {
     const [messages, setMessages] = useState([]);
@@ -21,13 +22,69 @@ const Channel = ({ user = null }) => {
         return unsubscribe;
 
     }, [])
+
+    const { uid, displayName, photoURL } = user;
+    const [newMessage, setNewMessage] = useState('');
+
+    const handleMessageOnChange = (e) => {
+        e.preventDefault();
+        setNewMessage(e.target.value);
+    }
+
+    const messagesRef = db.collection('messages');
+
+
+    const handleOnSubmit = e => {
+        e.preventDefault();
+
+        const trimmedMessage = newMessage.trim();
+        if (trimmedMessage) {
+            messagesRef.add({
+                text: trimmedMessage,
+                createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+                uid,
+                displayName,
+                photoURL,
+            });
+            setNewMessage('');
+        }
+    };
+    const inputRef = useRef();
+    useEffect(() => {
+        if (inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, [inputRef]);
     
     return (
-        <ul>
-            {messages.map(message => (
-                <li key={message.id}>{message.text}</li>
-            ))}
-        </ul>
+        <>
+            <ul>
+                {messages.map(message => (
+                    <li key={message.id}>
+                        <Message {...message} />
+                    </li>
+                ))}
+            </ul>
+
+            <form
+                onSubmit={handleOnSubmit}>
+                <input
+                    ref={inputRef}
+                    type="text"
+                    value={newMessage}
+                    onChange={handleMessageOnChange}
+                    placeholder="Escribe tu mensaje aqui..."
+                />
+
+                <button
+                    class=" enviar"
+                    type="submit"
+                    disabled={!newMessage}
+                >
+                    Enviar
+        </button>
+            </form>
+        </>
     );
 };
 
---
+++ src/Components/Message.js
@@ -0,0 +1,46 @@
+import { formatRelative } from 'date-fns';
+
+
+const formatDate = date => {
+    let formattedDate = '';
+    if (date) {
+        formattedDate = formatRelative(date, new Date());
+        formattedDate =
+            formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
+    }
+    return formattedDate;
+};
+
+const Message = ({
+    createdAt = null,
+    text = '',
+    displayName = '',
+    photoURL = '',
+}) => {
+    if (!text) return null;
+
+    
+    return (
+        <div class="todo">
+            
+            {photoURL ? (
+                <img
+                    src={photoURL}
+                    alt="Avatar"
+                    className="rounded-full mr-4"
+                    width={45}
+                    height={45}
+                />
+            ) : null}
+            
+            {displayName ? <p >{displayName}</p> : null}
+            {createdAt?.seconds ? (
+                <span>{formatDate(new Date(createdAt.seconds * 1000))}</span>
+            ) : null}
+            <div class="text"><p class="hola">{text}</p></div>
+        </div>
+        
+    );;
+};
+
+export default Message;
